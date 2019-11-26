4c8a23404f83a9473d4751d8c555330dce05681c
Jackie098 <carlosmiranda19122@gmail.com>
2019-11-26 10:05:18 -0300

    feat: rota de consulta de veiculos

--- app.js
+++ app.js
@@ -2,7 +2,24 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+let listCar = [
+    carro1 = {
+        cor: "Vermelho",
+        combus: "Gasolina"
+    },
+    carro2 = {
+        cor: "Roxo",
+        combus: "Alcool"
+    }
+];
+
 app.get('/', (req, res) => res.send('Hello World'));
+
+app.get('/carros', (req, res) => {
+    // listCar.map( todo => `<h2>${todo}</h2>`);
+
+    res.send(JSON.stringify(listCar));
+});
 
 
 app.listen(port, () => console.log("Server running -> http://localhost:3000"));
