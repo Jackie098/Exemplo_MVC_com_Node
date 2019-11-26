ab0df2c7940fddee84e71befa6604febba496e00
Jackie098 <carlosmiranda19122@gmail.com>
2019-11-26 10:48:50 -0300

    bug: Rota para inserir carros (POST)

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-let listCar = [
+var listCar = [
     carro1 = {
         cor: "Vermelho",
         combus: "Gasolina"
@@ -18,8 +18,21 @@ app.get('/', (req, res) => res.send('Hello World'));
 app.get('/carros', (req, res) => {
     // listCar.map( todo => `<h2>${todo}</h2>`);
 
+    //return res.json(listCar);
     res.send(JSON.stringify(listCar));
 });
+
+///carro/:cor/:combus
+app.post('/carro', (req, res) => {
+    let carro = {
+        cor: req.body.cor,
+        combus: req.body.combus
+    };
+    
+    listCar.push(carro);
+
+    return res.json(listCar);
+})
 
 
 app.listen(port, () => console.log("Server running -> http://localhost:3000"));
