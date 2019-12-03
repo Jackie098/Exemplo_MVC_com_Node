b620422dab42745217694b9f089345ec1242de7b
Jackie098 <carlosmiranda19122@gmail.com>
2019-12-03 19:54:39 -0300

    fix : Uso de promises na inserção de valores

--- models/Carro.js
+++ models/Carro.js
@@ -3,22 +3,22 @@ const pool = require('../connection');
 let Carro = function () {} 
 
 Carro.prototype.addNewCar = function(placa, modelo, cor, combus){
-        this.placa = placa;
-        this.modelo = modelo;
-        this.cor = cor;
-        this.combus = combus;
+    this.placa = placa;
+    this.modelo = modelo;
+    this.cor = cor;
+    this.combus = combus;
 
-        let query = `INSERT INTO carros VALUES 
-            (default, '${this.placa}', '${this.modelo}', '${this.cor}', '${this.combus}')`;
+    let query = `INSERT INTO carros VALUES 
+        (default, '${this.placa}', '${this.modelo}', '${this.cor}', '${this.combus}')`;
     
-        pool.query(query, (err) => {
-            return new Promise((resolve, reject) => {
-                if(err)
-                   reject(`Erro ao inserir carro: ${err}`);
-                else
-                   resolve(`Inserção OK`);
-            });
+    return new Promise((resolve, reject) => {
+        pool.query(query, (err) => {    
+            if(err)
+               reject(`Erro ao inserir carro: ${err}`);
+            else
+               resolve(`Inserção OK`);
         });
+    });
 }
 
 Carro.prototype.removeCar = function(placa){
